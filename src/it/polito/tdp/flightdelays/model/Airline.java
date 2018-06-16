a4270f0218ebc743a385777902ce39559138b689
Salvatore <Salvatore@192.168.1.138>
2018-06-16 08:14:46 +0200

    Implementazione dei metodi hashCode e equals

--- src/it/polito/tdp/flightdelays/model/Airline.java
+++ src/it/polito/tdp/flightdelays/model/Airline.java
@@ -30,4 +30,31 @@ public class Airline {
 	public String toString() {
 		return this.name;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Airline other = (Airline) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/it/polito/tdp/flightdelays/model/Flight.java
+++ src/it/polito/tdp/flightdelays/model/Flight.java
@@ -148,4 +148,29 @@ public class Flight {
 		builder.append("]");
 		return builder.toString();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Flight other = (Flight) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
 }
